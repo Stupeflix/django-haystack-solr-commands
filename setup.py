014616b21cb0cc57fa5ae51bf7e32b610825b700
Elias <elias@stupeflix.com>
2015-08-04 15:18:41 +0200

    better setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,33 @@
-from setuptools import setup, find_packages
+import os
+from setuptools import setup
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
 setup(
-    name = "haystack_solr_commands",
+    name = "django-haystack-solr-commands",
     version = "0.1",
-    packages = find_packages(),
+    author = "Elias Showk",
+    author_email = "elias@showk.me",
+    description = ("django management command 'solr' all-in-one for Solr 5"),
+    license = "BSD",
+    keywords = "solr haystack django management",
+    url = "https://github.com/elishowk/django-haystack-solr-commands",
+    packages=['haystack_solr_commands'],
+    long_description=read('README.md'),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: BSD License",
+    ],
+    dependency_links=[
+        "git+https://github.com/Stupeflix/django-haystack.git@master#egg=django-haystack"
+    ],
+    install_requires=[
+        "Django>=1.4"
+    ]
 )
